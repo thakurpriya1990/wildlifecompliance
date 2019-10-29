f29d6f100636ac4729682f89b61d8ba0d16c22dc
Katsu <katsufumi.shibata@dbca.wa.gov.au>
2019-10-29 14:30:39 +0800

    Payment status of infringement notice changes to paid after payment

--- wildlifecompliance/components/wc_payments/views.py
+++ wildlifecompliance/components/wc_payments/views.py
@@ -133,6 +133,10 @@ class InfringementPenaltySuccessView(TemplateView):
 
             order = Order.objects.get(basket=basket[0])
             invoice = Invoice.objects.get(order_number=order.number)
+            if invoice.payment_status == 'paid':
+                sanction_outcome.status = SanctionOutcome.STATUS_CLOSED
+                sanction_outcome.payment_status = SanctionOutcome.PAYMENT_STATUS_PAID
+                sanction_outcome.save()
             invoice_ref = invoice.reference
             fee_inv, created = InfringementPenaltyInvoice.objects.get_or_create(infringement_penalty=infringement_penalty, invoice_reference=invoice_ref)
 
