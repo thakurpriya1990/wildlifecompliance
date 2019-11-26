c1fd04ca2f4b54a2083d64d7e0f94d011b4d1989
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2019-11-26 16:23:21 +0800

    Fix a bug where infringement notice pdf cannot be displayed

--- ledger/payments/mixins.py
+++ ledger/payments/mixins.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import PermissionDenied
 
 from ledger.payments import helpers
+from wildlifecompliance.helpers import is_able_to_view_sanction_outcome_pdf
+
 
 class InvoiceOwnerMixin(object):
     
@@ -31,3 +33,27 @@ class InvoiceOwnerMixin(object):
         if not self.check_owner(request.user):    
             raise PermissionDenied
         return super(InvoiceOwnerMixin, self).dispatch(request, *args, **kwargs)
+
+
+class SanctionOutcomePdfMixin(object):
+    def check_owner(self, user):
+        ret_val = False
+        if is_able_to_view_sanction_outcome_pdf(user):
+            ret_val = True
+        else:
+            obj = self.get_object()
+            if hasattr(obj, 'order'):
+                if obj.order.user == user:
+                    ret_val = True
+            if hasattr(obj, 'offender'):
+                if obj.offender.person == user:
+                    ret_val = True
+
+        return ret_val
+
+        # return self.get_object().order.user == user or self.is_payment_admin(user)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.check_owner(request.user):
+            raise PermissionDenied
+        return super(SanctionOutcomePdfMixin, self).dispatch(request, *args, **kwargs)
