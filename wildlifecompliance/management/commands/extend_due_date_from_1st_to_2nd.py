a57a9a29e33a5de1495d33a5ad619445786a9b6b
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-01-15 10:56:28 +0800

    Refactor filterings for a cron job

--- wildlifecompliance/management/commands/extend_due_date_from_1st_to_2nd.py
+++ wildlifecompliance/management/commands/extend_due_date_from_1st_to_2nd.py
@@ -28,95 +28,78 @@ class Command(BaseCommand):
                 today = timezone.localtime(timezone.now()).date()
 
                 # Retrieve sanction outcomes whose type is Infringement Notice and which is unpaid
-                sanction_outcomes = SanctionOutcome.objects.filter(
+                sanction_outcomes_base = SanctionOutcome.objects\
+                    .filter(
                     Q(type=SanctionOutcome.TYPE_INFRINGEMENT_NOTICE) &
                     Q(status=SanctionOutcome.STATUS_AWAITING_PAYMENT) &
-                    Q(payment_status=SanctionOutcome.PAYMENT_STATUS_UNPAID))
-
-                # Conditions for filter the SanctionOutcomeDueDate
-                due_date_condition = (
-                    Q(due_date_1st__lt=today) &
-                    Q(due_date_term_currently_applied='1st') &
-                    Q(sanction_outcome__in=sanction_outcomes))
-
-                # Final query
-                # retrieve all the sanction_outcomes which expires 1st due date
-                due_dates = SanctionOutcomeDueDate.objects.filter(due_date_condition).\
-                    values('sanction_outcome').annotate(max_id=Max('id')).order_by('sanction_outcome').distinct()
+                    Q(payment_status=SanctionOutcome.PAYMENT_STATUS_UNPAID))\
+                    .filter(due_dates__in=SanctionOutcomeDueDate.objects.filter(Q(due_date_1st__lt=today) & Q(due_date_term_currently_applied='1st')))\
+                    .exclude(due_dates__in=SanctionOutcomeDueDate.objects.filter(Q(due_date_term_currently_applied='2nd')))
 
                 if DEBUG:
                     # For debugging purpose, infringement notice which has the string '__overdue1st__' in the description field is also selected.
                     logger.info('DEBUG = True')
-                    sanction_outcomes = SanctionOutcome.objects.filter(
+                    sanction_outcomes_debug = SanctionOutcome.objects.filter(
                         Q(type=SanctionOutcome.TYPE_INFRINGEMENT_NOTICE) &
                         Q(status=SanctionOutcome.STATUS_AWAITING_PAYMENT) &
                         Q(payment_status=SanctionOutcome.PAYMENT_STATUS_UNPAID) &
-                        Q(description__icontains='__overdue1st__'))
-
-                    # Conditions for filter the SanctionOutcomeDueDate
-                    due_date_condition = (
-                        Q(due_date_term_currently_applied='1st') &
-                        Q(sanction_outcome__in=sanction_outcomes))
-
-                    # Final query
-                    # retrieve all the sanction_outcomes which expires 1st due date
-                    due_dates_dev = SanctionOutcomeDueDate.objects.filter(due_date_condition). \
-                        values('sanction_outcome').annotate(max_id=Max('id')).order_by('sanction_outcome').distinct()
+                        Q(description__icontains='__overdue1st__')) \
+                        .filter(due_dates__in=SanctionOutcomeDueDate.objects.filter(Q(due_date_term_currently_applied='1st')))\
+                        .exclude(due_dates__in=SanctionOutcomeDueDate.objects.filter(Q(due_date_term_currently_applied='2nd')))
 
                 # Merge querysets
-                due_dates = due_dates | due_dates_dev if due_dates_dev else due_dates
-                due_dates = due_dates.distinct()
+                sanction_outcomes = (sanction_outcomes_base | sanction_outcomes_debug).distinct()
 
-                count = due_dates.count()
+                count = sanction_outcomes.count()
                 logger.info('{} overdue (1st) infringement notice(s) found.'.format(str(count)))
 
-                if count:
-                    # Create another due-date record
-                    for dict_item in due_dates:
-                        latest_due_date = SanctionOutcomeDueDate.objects.get(id=dict_item.get('max_id'))
-                        overdue_sanction_outcome = SanctionOutcome.objects.get(id=dict_item.get('sanction_outcome'))
-                        data = {}
-                        data['due_date_1st'] = latest_due_date.due_date_1st
-                        data['due_date_2nd'] = latest_due_date.due_date_2nd
-                        data['reason_for_extension'] = 'overdue 1st due date'
-                        data['extended_by_id'] = None
-                        data['sanction_outcome_id'] = overdue_sanction_outcome.id
-                        data['due_date_term_currently_applied'] = '2nd'
-                        serializer = SaveSanctionOutcomeDueDateSerializer(data=data)
+                # Process each overdue sanction outcome
+                for overdue_sanction_outcome in sanction_outcomes.all():
+                    latest_due_date = SanctionOutcomeDueDate.objects.filter(sanction_outcome=overdue_sanction_outcome).order_by('id').last()
+
+                    # Create new due_date record
+                    data = {}
+                    data['due_date_1st'] = latest_due_date.due_date_1st
+                    data['due_date_2nd'] = latest_due_date.due_date_2nd
+                    data['reason_for_extension'] = 'overdue 1st due date'
+                    data['extended_by_id'] = None
+                    data['sanction_outcome_id'] = overdue_sanction_outcome.id
+                    data['due_date_term_currently_applied'] = '2nd'
+                    serializer = SaveSanctionOutcomeDueDateSerializer(data=data)
+                    serializer.is_valid(raise_exception=True)
+                    serializer.save()
+
+                    # Create comms log entry
+                    data = {'sanction_outcome': overdue_sanction_outcome.id}
+                    serializer = SanctionOutcomeCommsLogEntrySerializer(data=data)
+                    serializer.is_valid(raise_exception=True)
+                    comms_log_entry = serializer.save()
+
+                    # Determine the bcc
+                    members = get_infringement_notice_coordinators()
+                    bcc_list = [member.email for member in members] if members else [settings.NOTIFICATION_EMAIL]
+                    if overdue_sanction_outcome.responsible_officer:
+                        bcc_list.append(overdue_sanction_outcome.responsible_officer.email)
+
+                    # Email
+                    to_address = [overdue_sanction_outcome.get_offender()[0].email, ]
+                    cc = None
+                    bcc = bcc_list
+                    email_data = send_remind_1st_period_overdue_mail(to_address, overdue_sanction_outcome, comms_log_entry, cc, bcc)
+
+                    # Add communication log
+                    if email_data:
+                        serializer = SanctionOutcomeCommsLogEntrySerializer(instance=comms_log_entry, data=email_data, partial=True)
                         serializer.is_valid(raise_exception=True)
                         serializer.save()
 
-                        # Create comms log entry
-                        data = {'sanction_outcome': overdue_sanction_outcome.id}
-                        serializer = SanctionOutcomeCommsLogEntrySerializer(data=data)
-                        serializer.is_valid(raise_exception=True)
-                        comms_log_entry = serializer.save()
-
-                        # Determine the bcc
-                        members = get_infringement_notice_coordinators()
-                        bcc_list = [member.email for member in members] if members else [settings.NOTIFICATION_EMAIL]
-                        if overdue_sanction_outcome.responsible_officer:
-                            bcc_list.append(overdue_sanction_outcome.responsible_officer.email)
-
-                        # Email
-                        to_address = [overdue_sanction_outcome.get_offender()[0].email, ]
-                        cc = None
-                        bcc = bcc_list
-                        email_data = send_remind_1st_period_overdue_mail(to_address, overdue_sanction_outcome, comms_log_entry, cc, bcc)
-
-                        # Add communication log
-                        if email_data:
-                            serializer = SanctionOutcomeCommsLogEntrySerializer(instance=comms_log_entry, data=email_data, partial=True)
-                            serializer.is_valid(raise_exception=True)
-                            serializer.save()
-
-                        # Add action log
-                        overdue_sanction_outcome.log_user_action(SanctionOutcomeUserAction.ACTION_INCREASE_FEE_AND_EXTEND_DUE.format(
-                            overdue_sanction_outcome.penalty_amount_1st,
-                            overdue_sanction_outcome.penalty_amount_2nd,
-                            latest_due_date.due_date_1st,
-                            latest_due_date.due_date_2nd,
-                        ))
+                    # Add action log
+                    overdue_sanction_outcome.log_user_action(SanctionOutcomeUserAction.ACTION_INCREASE_FEE_AND_EXTEND_DUE.format(
+                        overdue_sanction_outcome.penalty_amount_1st,
+                        overdue_sanction_outcome.penalty_amount_2nd,
+                        latest_due_date.due_date_1st,
+                        latest_due_date.due_date_2nd,
+                    ))
 
                 logger.info('Command {} completed'.format(__name__))
 
