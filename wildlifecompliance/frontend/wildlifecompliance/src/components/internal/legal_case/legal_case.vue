2229f41d6feac42ba1e350298409e015f77b4947
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-02-06 11:37:31 +0800

    Add Court-Proceedings tab

--- wildlifecompliance/frontend/wildlifecompliance/src/components/internal/legal_case/legal_case.vue
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/internal/legal_case/legal_case.vue
@@ -103,6 +103,7 @@
                         <li class="nav-item active"><a data-toggle="tab" :href="'#'+runTab">Running Sheet</a></li>
                         <li class="nav-item"><a data-toggle="tab" :href="'#'+cTab" >Case Details</a></li>
                         <li class="nav-item"><a data-toggle="tab" :href="'#'+bTab" >Brief of Evidence</a></li>
+                        <li class="nav-item"><a data-toggle="tab" :href="'#'+cpTab" >Court Proceedings</a></li>
                         <li class="nav-item"><a data-toggle="tab" :href="'#'+rTab">Related Items</a></li>
                     </ul>
                     <div class="tab-content">
@@ -160,6 +161,9 @@
                         </div>
                         <div :id="bTab" class="tab-pane fade in">
                             <BriefOfEvidence />
+                        </div>
+                        <div :id="cpTab" class="tab-pane fade in">
+                            <CourtProceedings />
                         </div>
                         <div :id="rTab" class="tab-pane fade in">
                             <FormSection :formCollapse="false" label="Related Items">
@@ -271,6 +275,7 @@ import LegalCaseWorkflow from './legal_case_workflow'
 import TreeSelect from '@riophae/vue-treeselect'
 import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 import BriefOfEvidence from './brief_of_evidence';
+import CourtProceedings from './court_proceedings';
 
 
 export default {
@@ -295,6 +300,7 @@ export default {
             runTab: 'runTab'+this._uid,
             rTab: 'rTab'+this._uid,
             cTab: 'cTab'+this._uid,
+            cpTab: 'cpTab'+this._uid,
             bTab: 'bTab'+this._uid,
             current_schema: [],
             workflowBindId: '',
@@ -433,6 +439,7 @@ export default {
     LegalCaseWorkflow,
     TreeSelect,
     BriefOfEvidence,
+    CourtProceedings,
   },
   computed: {
     ...mapGetters('legalCaseStore', {
