5e7cd3654cfed03d7e516a627266cd48608ec1da
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-02-18 12:23:06 +0800

    Formatting pdf

--- wildlifecompliance/components/sanction_outcome/pdf_in_blue.py
+++ wildlifecompliance/components/sanction_outcome/pdf_in_blue.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 
 from decimal import Decimal as D
@@ -13,7 +14,7 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle, StyleSheet
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.pdfbase import pdfmetrics
-from reportlab.lib.units import inch
+from reportlab.lib.units import inch, mm
 from reportlab.lib import colors
 
 from django.core.files import File
@@ -32,7 +33,7 @@ BPAY_LOGO = os.path.join(settings.BASE_DIR, 'staticfiles', 'payments', 'img', 'B
 HEADER_MARGIN = 10
 HEADER_SMALL_BUFFER = 3
 
-PAGE_MARGIN = 20
+PAGE_MARGIN = 30
 PAGE_TOP_MARGIN = 200
 
 PAGE_WIDTH, PAGE_HEIGHT = A4
@@ -237,32 +238,72 @@ class BrokenLine(Flowable):
 
 def _create_invoice(invoice_buffer, sanction_outcome):
     every_page_frame = Frame(PAGE_MARGIN, PAGE_MARGIN, PAGE_WIDTH - 2 * PAGE_MARGIN,
-                             PAGE_HEIGHT - 2 * PAGE_MARGIN, id='EveryPagesFrame', showBoundary=1)
+                             PAGE_HEIGHT - 2 * PAGE_MARGIN, id='EveryPagesFrame', showBoundary=0)
     # every_page_template = PageTemplate(id='EveryPages', frames=[every_page_frame,], onPage=_create_header)
     every_page_template = PageTemplate(id='EveryPages', frames=[every_page_frame,],)
     doc = BaseDocTemplate(invoice_buffer, pageTemplates=[every_page_template], pagesize=A4)
 
-    elements = []
+    col_width = [40*mm, 60*mm, 80*mm,]
 
     # SPAN (col, row)
     invoice_table_style = TableStyle([
         ('VALIGN', (0, 0), (-1, -1), 'TOP'),
         ('GRID', (0, 0), (-1, -1), 1, colors.black),
         ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+
         ('SPAN', (0, 0), (1, 0)),
+
+        # Alleged offender
+        ('SPAN', (0, 1), (0, 5)),
         ('SPAN', (1, 1), (2, 1)),
         ('SPAN', (1, 2), (2, 2)),
-        ('SPAN', (0, 1), (0, 2)),
+        ('SPAN', (1, 3), (2, 3)),
+        ('SPAN', (1, 4), (2, 4)),
+        ('SPAN', (1, 5), (2, 5)),
+
+        # When
+        ('SPAN', (1, 6), (2, 6)),
+
+        # Where
+        ('SPAN', (1, 7), (2, 7)),
+
+        # Alleged offence
+        ('SPAN', (0, 8), (0, 11)),
+        ('SPAN', (1, 8), (2, 8)),
+        ('SPAN', (1, 9), (2, 9)),
+        ('SPAN', (1, 10), (2, 10)),
+        ('SPAN', (1, 11), (2, 11)),
+
+        # Officer issuing notice
+        ('SPAN', (0, 12), (0, 14)),
+        ('SPAN', (1, 12), (2, 12)),
+        ('SPAN', (1, 13), (2, 13)),
+        ('SPAN', (1, 14), (2, 14)),
+
+        # Date
+        ('SPAN', (1, 15), (2, 15)),
     ])
 
-    # boundary = BrokenLine(PAGE_WIDTH - 2 * (PAGE_MARGIN * 1.1))
+    invoice_table_style2 = TableStyle([
+        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        # Notice to alleged offender
+        ('SPAN', (0, 0), (0, 2)),
+        ('SPAN', (1, 0), (2, 0)),
+        ('SPAN', (1, 1), (2, 1)),
+        ('SPAN', (1, 2), (2, 2)),
+    ])
+
+
+    date_str = gap(10) + '/' + gap(10) + '/ 20'
 
 
     styles = StyleSheet1()
     styles.add(ParagraphStyle(name='Normal',
                               fontName='Times-Roman',
-                              fontSize=10,
-                              leading=12))
+                              fontSize=12,
+                              leading=22))
     styles.add(ParagraphStyle(name='BodyText',
                               parent=styles['Normal'],
                               spaceBefore=6))
@@ -273,21 +314,93 @@ def _create_invoice(invoice_buffer, sanction_outcome):
                               parent=styles['BodyText'],
                               fontName='Times-Bold'))
     # styles = getSampleStyleSheet()
-    elements = []
-    line01 = [[Paragraph('Biodiversity Conservation Act 2016', styles['Italic']), Paragraph('Infringement notice', styles['Bold'])],
+    data = []
+    data.append([[Paragraph('Biodiversity Conservation Act 2016', styles['Italic']), Paragraph('Infringement notice', styles['Bold'])],
               '',
-              [Paragraph('Infringement', styles['Normal']), Paragraph('notice no.', styles['Normal'])]]
-    line02 = [Paragraph('Alleged offender', styles['Bold']), Paragraph('Name: Family name', styles['Normal']), '']
-    line03 = ['', Paragraph('Given names', styles['Normal']), '']
+              [Paragraph('Infringement', styles['Normal']), Paragraph('notice no.', styles['Normal'])]])
 
-    data = [line01, line02, line03]
+    # Alleged offender
+    data.append([Paragraph('Alleged offender', styles['Bold']), Paragraph('Name: Family name', styles['Normal']), ''])
+    data.append(['', Paragraph(gap(12) + 'Given names', styles['Normal']), ''])
+    data.append(['', Paragraph(gap(12) + 'Date of Birth', styles['Normal']), ''])
+    data.append(['', [Paragraph('or', styles['Bold']), Paragraph('Body corporate name', styles['Normal']), Spacer(1, 25)], ''])
+    data.append(['', [Paragraph('Address', styles['Normal']), Spacer(1, 25), Paragraph('Postcode', styles['Normal'])], ''])
 
-    t = Table(data, style=invoice_table_style,)
-    elements.append(t)
-    # elements.append(boundary)
+    # When
+    data.append([Paragraph('When', styles['Bold']), Paragraph('Date' + date_str + gap(5) + 'Time' + gap(10) + 'am/pm', styles['Normal']), ''])
+
+    # Where
+    data.append([Paragraph('Where', styles['Bold']), [Paragraph('Location of offence', styles['Normal']), Spacer(1, 25)], ''])
+
+    # Alleged offence
+    data.append([Paragraph('Alleged offence', styles['Bold']),
+                 [
+                     Paragraph('Description of offence', styles['Normal']),
+                     Paragraph('<u>' + gap(127) + '</u>', styles['Normal']),
+                     Paragraph('<u>' + gap(127) + '</u>', styles['Normal']),
+                     Paragraph('<u>' + gap(127) + '</u>', styles['Normal']),
+                     Paragraph('<u>' + gap(127) + '</u>', styles['Normal']),
+                     Paragraph('<u>' + gap(127) + '</u>', styles['Normal']),
+                 ],
+                 ''])  # row index: 8
+    data.append(['', Paragraph('Is this a 2nd or subsequent offence? Yes &#x25A1; No &#x25A1;', styles['Normal']), ''])
+    data.append(['', [Paragraph('Biodiversity Conservation Act 2016 s.', styles['Normal']),
+                      Paragraph('or', styles['Normal']),
+                      Paragraph('Biodiversity Conservation Regulations 2018 r.', styles['Normal'])], ''])
+    data.append(['', Paragraph('Modified penalty: $', styles['Normal']), ''])
+
+    # Officer issuing notice
+    data.append([Paragraph('Officer issuing notice', styles['Bold']), Paragraph('Name', styles['Normal']), ''])  # row index: 12
+    data.append(['', Paragraph('Signature', styles['Normal']), ''])
+    data.append(['', Paragraph('Officer no.', styles['Normal']), ''])
+
+    # Date
+    data.append([Paragraph('Date', styles['Bold']), Paragraph('Date of notice:' + date_str, styles['Normal']), ''])
+
+    # Create 1st table
+    t1 = Table(data, style=invoice_table_style, colWidths=col_width)
+
+    data_tbl2 = []
+    # Notice to alleged offender  (1)
+    body = []
+    body.append(Paragraph('It is alleged that you have committed the above offence.</p>', styles['Normal']))
+    body.append(Paragraph('If you do not want to be prosecuted in court for the offence, pay the modified penalty within 28 days after the date of this notice.', styles['Normal']))
+    body.append(Paragraph('How to pay', styles['Bold']))
+    body.append(Paragraph('<strong>By post:</strong>Send a cheque or money order (payable to ‘Approved Officer — Biodiversity Conservation Act 2016’) to:', styles['Normal']))
+    body.append(Paragraph('Approved Officer — Biodiversity Conservation Act 2016<br />Department of Biodiversity, Conservation and Attractions<br />Locked Bag 104<br />Bentley Delivery Centre WA 6983', styles['Normal']))
+    body.append(Spacer(1, 10))
+    body.append(Paragraph('<strong>In person:</strong> Pay the cashier at any office of the Department of Biodiversity, Conservation and Attractions, or pay over the telephone by credit card by calling the general telephone number of any office of the Department of Biodiversity, Conservation and Attractions.', styles['Normal']))
+    data_tbl2.append([Paragraph('Notice to alleged offender', styles['Bold']), body, ''])
+
+    # Notice to alleged offender  (2)
+    body = []
+    body.append(Paragraph('<strong>If you do not pay</strong> the modified penalty within 28 days, you may be prosecuted or enforcement action may be taken under the Fines, Penalties and Infringement Notices Enforcement Act 1994. Under that Act, some or all of the following action may be taken — your driver’s licence may be suspended; your vehicle licence may be suspended or cancelled; your details may be published on a website; your vehicle may be immobilised or have its number plates removed; your property may be seized and sold.', styles['Normal']))
+    body.append(Spacer(1, 10))
+    body.append(Paragraph('<strong>If you need more time.</strong> to pay the modified penalty, you can apply for an extension of time by writing to the Approved Officer at the above postal address.', styles['Normal']))
+    data_tbl2.append(['', body, ''])
+
+    # Notice to alleged offender  (3)
+    body = []
+    body.append(Paragraph('<strong>If you want this matter to be dealt with by prosecution in court</strong>, sign here <u>' + gap(80) + '</u> and post this notice to the Approved Officer at the above postal address within 28 days after the date of this notice.', styles['Normal']))
+    data_tbl2.append(['', body, ''])
+
+    # Create 2nd table
+    t2 = Table(data_tbl2, style=invoice_table_style2, colWidths=col_width)
+
+    elements = []
+    elements.append(t1)
+    elements.append(PageBreak())
+    elements.append(t2)
 
     doc.build(elements)
     return invoice_buffer
+
+
+def gap(num):
+    ret = ''
+    for i in range(num):
+        ret = ret + '&nbsp;'
+    return ret
 
 
 def create_in_pdf_bytes(filename, sanction_outcome):
