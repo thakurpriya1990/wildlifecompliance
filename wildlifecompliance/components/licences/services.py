2eb15a23d355cf124e992d7920bc5ced0df30768
sharpeez <shaynejmsharpe@gmail.com>
2020-07-30 12:15:55 +0800

    WildlifeCompliance: FIX licence actions for renew.

--- wildlifecompliance/components/licences/services.py
+++ wildlifecompliance/components/licences/services.py
@@ -5,6 +5,8 @@ from datetime import date
 
 from django.core.exceptions import ValidationError
 from django.db import transaction
+from django.db.models import Q
+from django.utils import timezone
 
 from wildlifecompliance.components.main.models import (
     GlobalSettings,
@@ -22,6 +24,7 @@ from wildlifecompliance.components.licences.email import (
 )
 from wildlifecompliance.components.applications.models import (
     Application,
+    ApplicationSelectedActivity,
     ApplicationSelectedActivityPurpose,
     ApplicationUserAction,
 )
@@ -204,101 +207,18 @@ class LicenceService(object):
         Get a list of activities for the licence, merged by application per
         licence_activity_id (1 per LicenceActivity).
         '''
-        latest_activities = licence.latest_activities
+        the_list = None
+        try:
+            on_licence_actioner = LicenceActioner(licence)
+            the_list = on_licence_actioner.get_latest_activities_for_request()
 
-        # new_activities = [
-        #     a for a in latest_activities
-        #     if a.application.application_type in [
-        #         'new_activity', 'new_licence']
-        # ]
+        except Exception as e:
+            logger.error('ERR get_activities_list_for() ID {0}: {1}'.format(
+                licence.id, e
+            ))
+            raise Exception('Failed getting activities for licence.')
 
-        # merge_ids = [
-        #     a.id for a in latest_activities
-        #     if a.application.application_type in [
-        #         'amend_activity', 'renew_activity', 'system_generated'] and
-        #     a.licence_activity_id in [
-        #         n.licence_activity_id for n in new_activities
-        #     ]
-        # ]
-
-        new_ids = [
-            a.id for a in latest_activities
-        ]
-
-        merged_activities = {}
-
-        if licence.is_latest_in_category:
-            purposes_in_open_applications = list(
-                licence.get_purposes_in_open_applications())
-        else:
-            purposes_in_open_applications = None
-
-        for activity in latest_activities:
-
-            if purposes_in_open_applications or\
-                    purposes_in_open_applications == []:
-
-                # TODO: can_action need to be done on purposes instead of
-                # activity.
-                activity_can_action = activity.can_action(
-                    purposes_in_open_applications
-                )
-
-            else:
-                activity_can_action = {
-                    'licence_activity_id': activity.licence_activity_id,
-                    'can_renew': False,
-                    'can_amend': False,
-                    'can_surrender': False,
-                    'can_cancel': False,
-                    'can_suspend': False,
-                    'can_reissue': False,
-                    'can_reinstate': False,
-                }
-
-            # Check if a record for the licence_activity_id already exists, if
-            # not, add.
-            if activity.id in new_ids:
-                issued_list = [
-                    p for p in activity.proposed_purposes.all() if p.is_issued]
-
-                if not len(issued_list):
-                    continue
-
-                merged_activities[activity] = {
-                    'id': activity.id,
-                    'licence_activity_id': activity.licence_activity_id,
-                    'activity_name_str': activity.licence_activity.name,
-                    'issue_date': activity.get_issue_date(),
-                    'start_date': activity.get_start_date(),
-                    'expiry_date': '\n'.join([
-                        '{}'.format(p.expiry_date.strftime(
-                            '%d/%m/%Y') if p.expiry_date else '')
-                        for p in activity.proposed_purposes.all()
-                    ]),
-                    'activity_purpose_names_and_status': '\n'.join([
-                        '{} ({})'.format(p.purpose.name, p.purpose_status)
-                        for p in activity.proposed_purposes.all()
-                    ]),
-                    'can_action':
-                        {
-                            'licence_activity_id':
-                                activity.licence_activity_id,
-                            'can_renew': activity_can_action['can_renew'],
-                            'can_amend': activity_can_action['can_amend'],
-                            'can_surrender':
-                                activity_can_action['can_surrender'],
-                            'can_cancel': activity_can_action['can_cancel'],
-                            'can_suspend': activity_can_action['can_suspend'],
-                            'can_reissue': activity_can_action['can_reissue'],
-                            'can_reinstate':
-                                activity_can_action['can_reinstate'],
-                        }
-                }
-
-        merged_activities_list = merged_activities.values()
-
-        return merged_activities_list
+        return the_list
 
     @staticmethod
     def verify_licence_renewals(request=None):
@@ -508,6 +428,49 @@ class LicenceActioner(LicenceActionable):
     def __str__(self):
         return 'LicenceActioner for {0}'.format(self.licence.id)
 
+    @staticmethod
+    def get_current_activities_for_application_type(
+            application_type, **kwargs):
+        """
+        Retrieves the current or suspended activities for an
+        ApplicationSelectedActivity, filterable by LicenceActivity ID and
+        Application.APPLICATION_TYPE in the case of the additional date_filter
+        (use Application.APPLICATION_TYPE_SYSTEM_GENERATED for no
+        APPLICATION_TYPE filters)
+        """
+        applications = kwargs.get('applications', Application.objects.none())
+        activity_ids = kwargs.get('activity_ids', [])
+        activities = None
+
+        date_filter = Application.get_activity_date_filter(
+            application_type)
+
+        ACCEPT = ApplicationSelectedActivity.PROCESSING_STATUS_ACCEPTED
+        # TODO: date_filter applies expiry_date
+        activities = ApplicationSelectedActivity.objects.filter(
+            Q(id__in=activity_ids) if activity_ids else
+            Q(application_id__in=applications.values_list('id', flat=True)),
+            # **date_filter
+        ).filter(
+            processing_status=ACCEPT
+        ).exclude(
+            activity_status__in=[
+                ApplicationSelectedActivity.ACTIVITY_STATUS_SURRENDERED,
+                ApplicationSelectedActivity.ACTIVITY_STATUS_EXPIRED,
+                ApplicationSelectedActivity.ACTIVITY_STATUS_CANCELLED,
+                ApplicationSelectedActivity.ACTIVITY_STATUS_REPLACED
+            ]
+        ).distinct()
+
+        a_ids = ApplicationSelectedActivityPurpose.objects.filter(
+            # Apply the date filtering on the Purposes.
+            selected_activity_id__in=[a.id for a in activities],
+            **date_filter
+        ).values_list('selected_activity_id', flat=True)
+        activities = activities.filter(id__in=a_ids)
+
+        return activities
+
     def get_actioned_purposes(self):
         '''
         Get a list of purposes which were actioned by this actioner.
@@ -535,6 +498,270 @@ class LicenceActioner(LicenceActionable):
         ]
 
         self.actioned_purposes = purposes
+
+    def can_action_purposes(self, purpose_list, activity):
+        '''
+        Applies a specified action to all licence purposes. For a selected
+        licence activity_id and purposes list, the selected activity status
+        will not be updated to allow further management of the activity.
+
+        Returns a DICT object containing can_<action> Boolean results of each
+        action check.
+        '''
+        SUSPENDED = ApplicationSelectedActivity.ACTIVITY_STATUS_SUSPENDED
+        EXPIRED = ApplicationSelectedActivity.ACTIVITY_STATUS_EXPIRED
+        ACCEPTED = ApplicationSelectedActivity.PROCESSING_STATUS_ACCEPTED
+
+        can_action = {
+            'licence_activity_id': activity.licence_activity_id,
+            'can_amend': False,
+            'can_renew': False,
+            'can_reactivate_renew': False,
+            'can_surrender': False,
+            'can_cancel': False,
+            'can_suspend': False,
+            'can_reissue': False,
+            'can_reinstate': False,
+        }
+        current_date = timezone.now().date()
+
+        # return false for all actions if activity is not in latest licence.
+        if not activity.is_in_latest_licence:
+            return can_action
+
+        # No action should be available if all of an activity's purposes are in
+        # open applications check if there are any purposes in open
+        # applications (i.e. can action) return false for all actions if no
+        # purposes are still actionable.
+        activity_purposes = activity.purposes.values_list('id', flat=True)
+        if not len(list((set(activity_purposes) - set(purpose_list)))) > 0:
+            return can_action
+
+        # can_amend is true if the activity can be included in a Amendment
+        # Application Extra exclude for SUSPENDED due to get_current_activities
+        # for_application_type intentionally not excluding these as part of the
+        # default queryset.
+        current = \
+            self.get_current_activities_for_application_type(
+                Application.APPLICATION_TYPE_AMENDMENT,
+                activity_ids=[activity.id]
+            ).exclude(activity_status=SUSPENDED).count() > 0
+
+        can_action['can_amend'] = current
+
+        # can_renew is true if the activity can be included in a Renewal
+        # Application Extra exclude for SUSPENDED due to get_current_activities
+        # for_application_type intentionally not excluding these as part of the
+        # default queryset.
+        current = self.get_current_activities_for_application_type(
+            Application.APPLICATION_TYPE_RENEWAL,
+            activity_ids=[activity.id]
+        ).exclude(activity_status=SUSPENDED).count() > 0
+
+        renewable = [
+            p for p in activity.proposed_purposes.all() if p.is_renewable
+        ]
+        # can_renew when activity is current with renewable purposes.
+        can_action['can_renew'] = current and len(renewable)
+
+        # can_reactivate_renew is true if the activity has expired, excluding
+        # if it was surrendered or cancelled
+        current = ApplicationSelectedActivity.objects.filter(
+            Q(id=activity.id, expiry_date__isnull=False),
+            Q(expiry_date__lt=current_date) |
+            Q(activity_status=EXPIRED)
+        ).filter(
+            processing_status=ACCEPTED
+        ).exclude(
+            activity_status__in=[
+                ApplicationSelectedActivity.ACTIVITY_STATUS_SURRENDERED,
+                ApplicationSelectedActivity.ACTIVITY_STATUS_CANCELLED,
+                ApplicationSelectedActivity.ACTIVITY_STATUS_REPLACED
+            ]
+        ).count() > 0
+
+        can_action['can_reactivate_renew'] = current
+
+        # can_surrender is true if the activity is CURRENT or SUSPENDED
+        # disable if there are any open applications to maintain licence
+        # sequence data integrity.
+        current = self.get_current_activities_for_application_type(
+            Application.APPLICATION_TYPE_SYSTEM_GENERATED,
+            activity_ids=[activity.id]
+        ).exclude(activity_status=SUSPENDED).count() > 0
+
+        surrenderable = [
+            p for p in activity.proposed_purposes.all() 
+            if p.is_issued and p.is_active
+        ]
+
+        can_action['can_surrender'] = current and len(surrenderable)
+
+        # can_cancel is true if the activity is CURRENT or SUSPENDED disable if
+        # there are any open applications to maintain licence sequence data
+        # integrity.
+        current = self.get_current_activities_for_application_type(
+            Application.APPLICATION_TYPE_SYSTEM_GENERATED,
+            activity_ids=[activity.id]
+        ).exclude(activity_status=SUSPENDED).count() > 0
+
+        cancelable = [
+            p for p in activity.proposed_purposes.all()
+            if p.is_issued and p.is_active
+        ]
+
+        can_action['can_cancel'] = current and len(cancelable)
+
+        # can_suspend is true if the activity_status is CURRENT Extra exclude
+        # for SUSPENDED due to get_current_activities_for_application_type
+        # intentionally not excluding these as part of the default queryset
+        current = self.get_current_activities_for_application_type(
+            Application.APPLICATION_TYPE_SYSTEM_GENERATED,
+            activity_ids=[activity.id]
+        ).exclude(activity_status=SUSPENDED).count() > 0
+
+        suspendable = [
+            p for p in activity.proposed_purposes.all()
+            if p.is_issued and p.is_active
+        ]
+
+        can_action['can_suspend'] = current and len(suspendable)
+
+        # can_reissue is true if the activity can be included in a Reissue
+        # Application Extra exclude for SUSPENDED due to
+        # get_current_activities_for_application_type intentionally not
+        # excluding these as part of the default queryset disable if there are
+        # any open applications to maintain licence sequence data integrity.
+        if not purpose_list:
+            current = self.get_current_activities_for_application_type(
+                Application.APPLICATION_TYPE_REISSUE,
+                activity_ids=[activity.id]
+            ).exclude(
+                activity_status__in=[SUSPENDED]
+            ).count() > 0
+
+            reissuable = [
+                p for p in activity.proposed_purposes.all() if p.is_issued
+            ]
+
+            can_action['can_reissue'] = current and len(reissuable)
+
+        # can_reinstate is true if the activity has not yet expired and is
+        # currently SUSPENDED, CANCELLED or SURRENDERED.
+        # current = self.get_current_activities_for_application_type(
+        #     Application.APPLICATION_TYPE_RENEWAL,
+        #     activity_ids=[activity.id]
+        # ).exclude(activity_status=SUSPENDED).count() > 0
+
+        reinstatable = [
+            p for p in activity.proposed_purposes.all() if p.is_reinstatable
+        ]
+        can_action['can_reinstate'] = len(reinstatable)
+
+        return can_action
+
+    def get_latest_activities_for_request(self, request=None):
+        '''
+        Gets a list of current selected licence activities available on this
+        licence for an action request. The list is a merged set of new and
+        amended activities which can be actioned.
+
+        :return a list of actionable current selected licence activities.
+        '''
+        latest_activities = self.licence.latest_activities
+
+        # new_activities = [
+        #     a for a in latest_activities
+        #     if a.application.application_type in [
+        #         'new_activity', 'new_licence']
+        # ]
+
+        # merge_ids = [
+        #     a.id for a in latest_activities
+        #     if a.application.application_type in [
+        #         'amend_activity', 'renew_activity', 'system_generated'] and
+        #     a.licence_activity_id in [
+        #         n.licence_activity_id for n in new_activities
+        #     ]
+        # ]
+
+        new_ids = [
+            a.id for a in latest_activities
+        ]
+
+        merged_activities = {}
+
+        if self.licence.is_latest_in_category:
+            purposes_in_open_applications = list(
+                self.licence.get_purposes_in_open_applications())
+        else:
+            purposes_in_open_applications = None
+
+        for activity in latest_activities:
+
+            if purposes_in_open_applications or\
+                    purposes_in_open_applications == []:
+
+                activity_can_action = self.can_action_purposes(
+                    purposes_in_open_applications,
+                    activity,
+                )
+
+            else:
+                activity_can_action = {
+                    'licence_activity_id': activity.licence_activity_id,
+                    'can_renew': False,
+                    'can_amend': False,
+                    'can_surrender': False,
+                    'can_cancel': False,
+                    'can_suspend': False,
+                    'can_reissue': False,
+                    'can_reinstate': False,
+                }
+
+            # Check if a record for the licence_activity_id already exists, if
+            # not, add.
+            if activity.id in new_ids:
+                issued_list = [
+                    p for p in activity.proposed_purposes.all() if p.is_issued]
+
+                if not len(issued_list):
+                    continue
+
+                merged_activities[activity] = {
+                    'id': activity.id,
+                    'licence_activity_id': activity.licence_activity_id,
+                    'activity_name_str': activity.licence_activity.name,
+                    'issue_date': activity.get_issue_date(),
+                    'start_date': activity.get_start_date(),
+                    'expiry_date': '\n'.join([
+                        '{}'.format(p.expiry_date.strftime(
+                            '%d/%m/%Y') if p.expiry_date else '')
+                        for p in activity.proposed_purposes.all()
+                    ]),
+                    'activity_purpose_names_and_status': '\n'.join([
+                        '{} ({})'.format(p.purpose.name, p.purpose_status)
+                        for p in activity.proposed_purposes.all()
+                    ]),
+                    'can_action':
+                        {
+                            'licence_activity_id':
+                                activity.licence_activity_id,
+                            'can_renew': activity_can_action['can_renew'],
+                            'can_amend': activity_can_action['can_amend'],
+                            'can_surrender':
+                                activity_can_action['can_surrender'],
+                            'can_cancel': activity_can_action['can_cancel'],
+                            'can_suspend': activity_can_action['can_suspend'],
+                            'can_reissue': activity_can_action['can_reissue'],
+                            'can_reinstate':
+                                activity_can_action['can_reinstate'],
+                        }
+                }
+
+        merged_activities_list = merged_activities.values()
+
+        return merged_activities_list
 
     def get_latest_purposes_for_request(self, request):
         '''
