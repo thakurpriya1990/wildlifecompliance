5c90d23e7a32c45bcf0ec3f992034a30c6cc8fe3
sharpeez <shaynejmsharpe@gmail.com>
2020-11-12 12:14:48 +0800

    WildlifeCompliance: FIX unable to configure handler application_checkout error.

--- wildlifecompliance/settings.py
+++ wildlifecompliance/settings.py
@@ -1,11 +1,10 @@
-from django.core.exceptions import ImproperlyConfigured
-from ledger.settings_base import *
-
 import os
 import confy
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 confy.read_environment_file(BASE_DIR+"/.env")
 os.environ.setdefault("BASE_DIR", BASE_DIR)
+from django.core.exceptions import ImproperlyConfigured
+from ledger.settings_base import *
 
 os.environ['LEDGER_PRODUCT_CUSTOM_FIELDS'] = "('ledger_description','quantity','price_incl_tax','price_excl_tax','oracle_code')"
 os.environ['LEDGER_REFUND_TRANSACTION_CALLBACK_MODULE'] = 'wildlifecompliance:wildlifecompliance.components.applications.api.application_refund_callback'
