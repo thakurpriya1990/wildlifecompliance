5c055986a735a07125942fb19df7b393ebb765df
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2020-11-28 11:37:58 +0800

    fixed css pnel issue

--- wildlifecompliance/frontend/wildlifecompliance/src/components/forms/group2.vue
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/forms/group2.vue
@@ -1,73 +1,66 @@
 <template lang="html">
     <div class="top-buffer bottom-buffer">
 
-        <!--
-        <div v-for="(n,idx) in num_groups()" class="panel panel-default">
-        <div v-for="(n,idx) in 1" class="panel panel-default">
-        -->
-                <label :id="id" class="inline">{{label}}</label>
-                    <!--<i data-toggle="tooltip" v-if="help_text" data-placement="right" class="fa fa-question-circle" :title="help_text"> &nbsp; </i>-->
-                <template v-if="help_text">
-                    <HelpText :help_text="help_text" /> 
-                </template>
+        <label :id="id" class="inline">{{label}}</label>
+        <!--<i data-toggle="tooltip" v-if="help_text" data-placement="right" class="fa fa-question-circle" :title="help_text"> &nbsp; </i>-->
 
-                <template v-if="help_text_url">
-                    <HelpTextUrl :help_text_url="help_text_url" /> 
-                </template>
+        <template v-if="help_text">
+            <HelpText :help_text="help_text" /> 
+        </template>
+
+        <template v-if="help_text_url">
+            <HelpTextUrl :help_text_url="help_text_url" /> 
+        </template>
  
-		<div class="repeatable-group" v-for="(group, groupIdx) in repeatableGroups" 
-		    :id="`repeatable_group_${component.name}_${groupIdx}`"
-		    v-bind:key="`repeatable_group_${component.name}_${groupIdx}`">
-		    <div class="panel panel-default">
-			<div class="panel-body">
-			    <!--
-			    <p> name: {{name}} </p>
-			    <p> value: {{value}} </p>
-			    <p> component: {{component}} </p>
-			    <p> children: {{component.children}} </p>
-			    <p> {{groupIdx}} group: {{group}} </p>
-			    <p> {{groupIdx}} value: {{value}} </p>
-			    -->
+        <div class="repeatable-group" v-for="(group, groupIdx) in repeatableGroups" 
+            :id="`repeatable_group_${component.name}_${groupIdx}`"
+            v-bind:key="`repeatable_group_${component.name}_${groupIdx}`">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <!--
+                    <p> name: {{name}} </p>
+                    <p> value: {{value}} </p>
+                    <p> component: {{component}} </p>
+                    <p> children: {{component.children}} </p>
+                    <p> {{groupIdx}} group: {{group}} </p>
+                    <p> {{groupIdx}} value: {{value}} </p>
+                    <div :class="{'row':true,'collapse':true, 'in':isExpanded}" style="margin-top:10px;" >
+                    -->
 
-			    <a class="collapse-link-top pull-right" @click.prevent="expand(group)"><span class="glyphicon glyphicon-chevron-down"></span></a>
-			    <div class="children-anchor-point collapse in" style="padding-left: 0px"></div>
-			    <a class="collapse-link-bottom pull-right"  @click.prevent="minimize(group)"><span class="glyphicon glyphicon-chevron-up"></span></a>
+                    <a class="collapse-link-top pull-right" @click.prevent="expand(group)"><span class="glyphicon glyphicon-chevron-down"></span></a>
+                    <div class="children-anchor-point collapse in" style="padding-left: 0px"></div>
+                    <a class="collapse-link-bottom pull-right"  @click.prevent="minimize(group)"><span class="glyphicon glyphicon-chevron-up"></span></a>
 
-			    <div :class="{'row':true,'collapse':true, 'in':isExpanded}" style="margin-top:10px;" >
+                    <div :class="{'collapse':true, 'in':isExpanded}" style="margin-top:10px;" >
 
-  			      <div>
-				<div v-for="(subcomponent, index) in components[group].children"
-				    v-bind:key="`repeatable_group_subcomponent_${subcomponent.name}_${index}`">
+                        <div v-for="(subcomponent, index) in components[group].children"
+                            v-bind:key="`repeatable_group_subcomponent_${subcomponent.name}_${index}`">
 
-				    <!--
-				    <p> {{index}} subcomponent: {{subcomponent.name}} </p>
-				    <span v-if="!index" :class="`expand-icon ${isExpanded(group) ? 'collapse' : ''}`"
-					v-on:click="toggleGroupVisibility(group)"></span>
+                            <!--
+                            <p> {{index}} subcomponent: {{subcomponent.name}} </p>
+                            <span v-if="!index" :class="`expand-icon ${isExpanded(group) ? 'collapse' : ''}`"
+                            v-on:click="toggleGroupVisibility(group)"></span>
 
-					    :instance="group"
-				    <p> components: {{components}} </p>
-				    -->
+                                :instance="group"
+                            <p> components: {{components}} </p>
+                            -->
 
-				    <renderer-block
-					:component="subcomponent"
-					:json_data="value"
-					v-bind:key="`repeatable_group_subcomponent_contents_${subcomponent.name}_${index}`"
-				    />
+                            <renderer-block
+                                :component="subcomponent"
+                                :json_data="value"
+                                v-bind:key="`repeatable_group_subcomponent_contents_${subcomponent.name}_${index}`"
+                            />
 
-				    <div>
-					<!-- <p> {{ group }} </p> -->
-					<button v-if="groupIdx && index == component.children.length-1 && !readonly" type="button" class="btn btn-danger"
-					    @click.prevent="removeGroup(group)">Delete group</button>
-				    </div>
-				</div>
-			      </div>
+                            <div>
+                                <button v-if="groupIdx && index == component.children.length-1 && !readonly" type="button" class="btn btn-danger"
+                                    @click.prevent="removeGroup(group)">Delete group</button>
+                            </div>
+                        </div>
 
-			    </div>
-			</div>
-		    </div>
-		</div>
-
-	</div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <div class="row" v-if="component.isRepeatable && !readonly">
             <input type="button" value="Add Group" class="btn btn-primary add-new-button"
@@ -186,21 +179,21 @@ const Group2 = {
             /* search a nested JSON string for key, and recursively update the value using function fn 
                NOTE: had to use ES6 arrow function style, because the normal recursion function looped infinately inside vue instance.
 
-	       Usage: 
+           Usage: 
                     -- the below will search all k,v pairs in JSON object and append '-ridx0' (repeatable index 0) to all values with key='name'
                     var ridx = 0;
-  		    vm.components[group] = vm.updateComponent(vm.component, v => v + '-ridx' + ridx)
-	    */
+            vm.components[group] = vm.updateComponent(vm.component, v => v + '-ridx' + ridx)
+        */
             return Object.fromEntries(Object
                 .entries(json_obj)
                 .map(([k, v]) => [k,
-                    Array.isArray(v) 						// if
+                    Array.isArray(v)                        // if
                         ? Array.from(v, v => this.updateComponent(v, fn))
-                    : v && typeof v === 'object'				// elif
+                    : v && typeof v === 'object'                // elif
                         ? this.updateComponent(v, fn)
-                    : k===key							// elif
+                    : k===key                           // elif
                         ? fn(v)
-                    : v								// else
+                    : v                             // else
                 ])
             );
         },
@@ -212,14 +205,14 @@ const Group2 = {
             let vm = this;
             //return (f, t) =>
             const traverse = (f = identity, t = {}) =>
-		Array.isArray(t)
-		    ? Array.from(t, v => traverse(v, f))
-		: Object(t) === t
-		    ? Object.fromEntries(Object
-			.entries(t)
-			.map(([ k, v ]) =>  [k, v && typeof v === 'object' ? traverse(v, f) : (k==='name' ? f(v) : v)])
-		      ) 
-		: ''
+        Array.isArray(t)
+            ? Array.from(t, v => traverse(v, f))
+        : Object(t) === t
+            ? Object.fromEntries(Object
+            .entries(t)
+            .map(([ k, v ]) =>  [k, v && typeof v === 'object' ? traverse(v, f) : (k==='name' ? f(v) : v)])
+              ) 
+        : ''
 
              return traverse(x => x + '101', obj)
         },
@@ -271,13 +264,13 @@ const Group2 = {
 
 //    created:{
 //        traverse = (f = identity, t = {}) =>
-//	    Array.isArray(t)                         // 1
-//	        ? Array.from(t, v => this.traverse(f, v))
-//	    : Object(t) === t                        // 2
-//	        ? Object.fromEntries(Object
-//		    .entries(t)
-//		    .map(([ k, v ]) =>  [k, v && typeof v === 'object' ? this.traverse(f, v) : (k==='name' ? f(v) : v)])
-//	          )
+//      Array.isArray(t)                         // 1
+//          ? Array.from(t, v => this.traverse(f, v))
+//      : Object(t) === t                        // 2
+//          ? Object.fromEntries(Object
+//          .entries(t)
+//          .map(([ k, v ]) =>  [k, v && typeof v === 'object' ? this.traverse(f, v) : (k==='name' ? f(v) : v)])
+//            )
 //    },
 
     computed:{
