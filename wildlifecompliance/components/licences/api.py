cfaf016da1d305edca751cbdde98777a2070847d
sharpeez <shaynejmsharpe@gmail.com>
2020-12-10 13:41:20 +0800

    WLC #488.UPDATE licences for Py3 UTF-8 support.

--- wildlifecompliance/components/licences/api.py
+++ wildlifecompliance/components/licences/api.py
@@ -713,7 +713,8 @@ class LicenceViewSet(viewsets.ModelViewSet):
             if hasattr(e, 'error_dict'):
                 raise serializers.ValidationError(repr(e.error_dict))
             else:
-                raise serializers.ValidationError(repr(e[0].encode('utf-8')))
+                # raise serializers.ValidationError(repr(e[0].encode('utf-8')))
+                raise serializers.ValidationError(repr(e[0]))
         except Exception as e:
             print(traceback.print_exc())
             raise serializers.ValidationError(str(e))
