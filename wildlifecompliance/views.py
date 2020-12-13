2bba911579a9be70d39f2143bb6b099813972afb
sharpeez <shaynejmsharpe@gmail.com>
2020-12-13 12:27:29 +0800

    WLC #564.FIX SyntaxError at /mgt-commands.

--- wildlifecompliance/views.py
+++ wildlifecompliance/views.py
@@ -135,14 +135,46 @@ class ManagementCommandsView(LoginRequiredMixin, TemplateView):
     template_name = 'wildlifecompliance/mgt-commands.html'
 
     def post(self, request):
+        from wildlifecompliance.components.returns.services import (
+            ReturnService)
+        from wildlifecompliance.components.licences.services import (
+            LicenceService)
+        from wildlifecompliance.components.applications.services import (
+            ApplicationService)
+
         data = {}
         command_script = request.POST.get('script', None)
         if command_script:
             logger.info(
                 'ManagementCommandsView(): running - {0}'.format(
                     command_script
-            ))
-            call_command(command_script)
-            data.update({command_script: 'true'})
+                ))
+
+            # FIXME: call_command causes an unexpected EOF error with call to
+            # internal routine _bootsrap._gcd_import(name[level:], package).
+            # Temporary solution is to by-pass call_command, calling scripts
+            # directly.
+
+            # call_command(command_script)
+            # data.update({command_script: 'true'})
+
+            # call('python manage_wc.py verify_due_returns', shell=True)
+            if command_script == 'verify_due_returns':
+                ReturnService.verify_due_returns()
+
+            # call('python manage_wc.py verify_expired_licences', shell=True)
+            elif command_script == 'verify_expired_licences':
+                LicenceService.verify_expired_licences()
+
+            # call('python manage_wc.py verify_licence_renewals', shell=True)
+            elif command_script == 'verify_licence_renewals':
+                LicenceService.verify_expired_licences()
+
+            # call('python manage_wc.py verify_species', shell=True)
+            elif command_script == 'verify_licence_renewals':
+                ApplicationService.verify_licence_species()
+
+            else:
+                pass
 
         return render(request, self.template_name, data)
