04b41dfe4da632c6db77d85b253a4e72a64e14cb
sharpeez <shaynejmsharpe@gmail.com>
2021-05-26 11:47:20 +0800

    CLEANUP main api.

--- wildlifecompliance/components/main/api.py
+++ wildlifecompliance/components/main/api.py
@@ -1,39 +1,52 @@
+import traceback
+
 from wsgiref.util import FileWrapper
 
 from django.http import HttpResponse
+from django.db import transaction
+from django.core.exceptions import ValidationError
 
-from wildlifecompliance.components.call_email.models import Location
-from wildlifecompliance.components.call_email.serializers import LocationSerializer
-from wildlifecompliance.components.main.serializers import (
-    TemporaryDocumentCollectionSerializer,
-    BookingSettlementReportSerializer, OracleSerializer)
-from wildlifecompliance.components.main.models import TemporaryDocumentCollection
-from wildlifecompliance.components.main.process_document import save_document, cancel_document, delete_document
-from rest_framework import viewsets, serializers, status, generics, views, filters
-from rest_framework.decorators import (
-    detail_route,
-    list_route,
-    renderer_classes,
-    parser_classes,
-    api_view
-)
-
-from wildlifecompliance.components.wc_payments import reports
-from wildlifecompliance.helpers import is_internal
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from django.core.exceptions import ValidationError
-import traceback
-from django.db import transaction
+from rest_framework import viewsets
+from rest_framework import serializers
+# from rest_framework import status
+# from rest_framework import generics
+from rest_framework import views
+# from rest_framework import filters
+from rest_framework.decorators import detail_route
+# from rest_framework.decorators import list_route
+from rest_framework.decorators import renderer_classes
+# from rest_framework.decorators import parser_classes
+# from rest_framework.decorators import api_view
+
 from ledger.payments.utils import oracle_parser
 
-from wildlifecompliance.settings import WC_PAYMENT_SYSTEM_PREFIX, SYSTEM_NAME
+from wildlifecompliance.settings import WC_PAYMENT_SYSTEM_PREFIX
+# from wildlifecompliance.settings import SYSTEM_NAME
+from wildlifecompliance.components.call_email.models import Location
+from wildlifecompliance.components.call_email.serializers import (
+    LocationSerializer
+)
+from wildlifecompliance.components.main.serializers import (
+    TemporaryDocumentCollectionSerializer,
+    BookingSettlementReportSerializer, OracleSerializer
+)
+from wildlifecompliance.components.main.models import (
+    TemporaryDocumentCollection
+)
+from wildlifecompliance.components.main.process_document import save_document
+from wildlifecompliance.components.main.process_document import cancel_document
+from wildlifecompliance.components.main.process_document import delete_document
+from wildlifecompliance.components.wc_payments import reports
+from wildlifecompliance.helpers import is_internal
 
 
 def save_location(location_request_data, *args, **kwargs):
     try:
         if location_request_data.get('id'):
-            location_instance = Location.objects.get(id=location_request_data.get('id'))
+            location_instance = Location.objects.get(
+                id=location_request_data.get('id'))
             location_serializer = LocationSerializer(
                 instance=location_instance,
                 data=location_request_data,
@@ -71,7 +84,7 @@ class TemporaryDocumentCollectionViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         # import ipdb; ipdb.set_trace()
-        #user = self.request.user
+        # user = self.request.user
         if is_internal(self.request):
             return TemporaryDocumentCollection.objects.all()
         return TemporaryDocumentCollection.objects.none()
@@ -87,7 +100,10 @@ class TemporaryDocumentCollectionViewSet(viewsets.ModelViewSet):
                 serializer.is_valid(raise_exception=True)
                 if serializer.is_valid():
                     instance = serializer.save()
-                    save_document(request, instance, comms_instance=None, document_type=None)
+                    save_document(
+                        request,
+                        instance, comms_instance=None, document_type=None
+                    )
 
                     return Response(serializer.data)
         except serializers.ValidationError:
@@ -105,7 +121,8 @@ class TemporaryDocumentCollectionViewSet(viewsets.ModelViewSet):
 
     @detail_route(methods=['POST'])
     @renderer_classes((JSONRenderer,))
-    # Designed for uploading comms_log files within "create" modals when no parent entity instance yet exists
+    # Designed for uploading comms_log files within "create" modals when no
+    # parent entity instance yet exists
     # response returned to compliance_file.vue
     def process_temp_comms_log_document(self, request, *args, **kwargs):
         print("process_temp_comms_log_document")
@@ -113,19 +130,22 @@ class TemporaryDocumentCollectionViewSet(viewsets.ModelViewSet):
         try:
             instance = self.get_object()
             action = request.data.get('action')
-            #comms_instance = None
+            # comms_instance = None
 
             if action == 'list':
                 pass
 
             elif action == 'delete':
-                delete_document(request, instance, comms_instance=None, document_type=None)
+                delete_document(
+                    request, instance, comms_instance=None, document_type=None)
 
             elif action == 'cancel':
-                cancel_document(request, instance, comms_instance=None, document_type=None)
+                cancel_document(
+                    request, instance, comms_instance=None, document_type=None)
 
             elif action == 'save':
-                save_document(request, instance, comms_instance=None, document_type=None)
+                save_document(
+                    request, instance, comms_instance=None, document_type=None)
 
             returned_file_data = [dict(
                         file=d._file.url,
@@ -142,55 +162,74 @@ class TemporaryDocumentCollectionViewSet(viewsets.ModelViewSet):
 class BookingSettlementReportView(views.APIView):
     renderer_classes = (JSONRenderer,)
 
-    def get(self,request,format=None):
+    def get(self, request, format=None):
         try:
-            http_status = status.HTTP_200_OK
-            #parse and validate data
+            # http_status = status.HTTP_200_OK
+            # parse and validate data
             report = None
             data = {
-                "date":request.GET.get('date'),
+                "date": request.GET.get('date'),
             }
             serializer = BookingSettlementReportSerializer(data=data)
             serializer.is_valid(raise_exception=True)
-            filename = 'Booking Settlement Report-{}'.format(str(serializer.validated_data['date']))
+            filename = 'Booking Settlement Report-{}'.format(
+                str(serializer.validated_data['date'])
+            )
             # Generate Report
-            report = reports.booking_bpoint_settlement_report(serializer.validated_data['date'])
+            report = reports.booking_bpoint_settlement_report(
+                serializer.validated_data['date']
+            )
             if report:
-                response = HttpResponse(FileWrapper(report), content_type='text/csv')
-                response['Content-Disposition'] = 'attachment; filename="{}.csv"'.format(filename)
+                response = HttpResponse(
+                    FileWrapper(report), content_type='text/csv'
+                )
+                response[
+                    'Content-Disposition'
+                ] = 'attachment; filename="{}.csv"'.format(filename)
                 return response
             else:
                 raise serializers.ValidationError('No report was generated.')
         except serializers.ValidationError:
             raise
-        except Exception as e:
+        except Exception:
             traceback.print_exc()
 
 
 def oracle_integration(date, override):
     system = WC_PAYMENT_SYSTEM_PREFIX
-    #oracle_codes = oracle_parser(date, system, 'Commercial Operator Licensing', override=override)
-    oracle_codes = oracle_parser(date, system, 'WildlifeCompliance', override=override)
+    # oracle_codes = oracle_parser(
+    # date, system, 'Commercial Operator Licensing', override=override
+    # )
+    oracle_codes = oracle_parser(
+        date, system, 'WildlifeCompliance', override=override
+    )
 
 
 class OracleJob(views.APIView):
-    renderer_classes = [JSONRenderer,]
+    renderer_classes = [JSONRenderer, ]
+
     def get(self, request, format=None):
         try:
             data = {
-                "date":request.GET.get("date"),
+                "date": request.GET.get("date"),
                 "override": request.GET.get("override")
             }
             serializer = OracleSerializer(data=data)
             serializer.is_valid(raise_exception=True)
-            oracle_integration(serializer.validated_data['date'].strftime('%Y-%m-%d'),serializer.validated_data['override'])
-            data = {'successful':True}
+            oracle_integration(
+                serializer.validated_data[
+                    'date'
+                ].strftime('%Y-%m-%d'), serializer.validated_data['override']
+            )
+            data = {'successful': True}
             return Response(data)
         except serializers.ValidationError:
             print(traceback.print_exc())
             raise
         except ValidationError as e:
-            raise serializers.ValidationError(repr(e.error_dict)) if hasattr(e, 'error_dict') else serializers.ValidationError(e)
+            raise serializers.ValidationError(repr(e.error_dict)) if hasattr(
+                e, 'error_dict'
+            ) else serializers.ValidationError(e)
         except Exception as e:
             print(traceback.print_exc())
             raise serializers.ValidationError(str(e[0]))
