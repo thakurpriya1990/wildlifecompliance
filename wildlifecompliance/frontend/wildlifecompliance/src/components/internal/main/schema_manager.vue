f13d26e25a751e6ff3fe5028baa0a02c02c12d8e
sharpeez <shaynejmsharpe@gmail.com>
2021-06-02 10:56:46 +0800

    WLC #1129.ADD base components for Schema generation Admin Option.

---
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/internal/main/schema_manager.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container" id="schema-manager">
+    <div class="col-md-12">
+
+    <SchemaTabs :tabs="tabs" :initialTab="initialTab">
+
+    </SchemaTabs>
+
+    </div>
+</div>
+</template>
+<script>
+import SchemaTabs from '@/components/forms/tab.vue'
+
+export default {
+    name: 'schema-manager',
+    components: {
+        SchemaTabs,
+    },
+    data() {
+        return {
+            initialTab: 'masterlist',
+            tabs: [
+
+            ],
+        }
+    },
+}
+</script>
