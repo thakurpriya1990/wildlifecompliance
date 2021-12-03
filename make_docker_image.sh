d4b62ee486fcd53ec6bd27d35f4546470858c8ee
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-12-03 11:28:47 +0800

    Update make_docker_image.sh

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+## sole parameter is an integer indicating incremental daily version
+BUILD_TAG=dbcawa/wildlifecompliance:v$(date +%Y.%m.%d).$1
+git checkout dbca_compliance_mgt_dev &&
+git pull &&
 cd wildlifecompliance/frontend/wildlifecompliance/ &&
 npm run build &&
 cd ../../../ &&
 source venv/bin/activate &&
 ./manage_wc.py collectstatic --no-input &&
 git log --pretty=medium -30 > ./wlc_git_history &&
-docker image build --no-cache --tag $1 .
+docker image build --no-cache --tag $BUILD_TAG . &&
+git checkout working
+echo $BUILD_TAG
