ced342c61020a2ced6fbdc61d026aa66b2b5b632
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-01-13 08:49:19 +0800

    add error handling for make_docker_image.sh

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -2,14 +2,19 @@
 ## sole parameter is an integer indicating incremental daily version
 BUILD_TAG=dbcawa/wildlifecompliance:v$(date +%Y.%m.%d).$1
 git checkout dbca_compliance_mgt_dev &&
-git pull &&
-cd wildlifecompliance/frontend/wildlifecompliance/ &&
-npm run build &&
-cd ../../../ &&
-source venv/bin/activate &&
-./manage_wc.py collectstatic --no-input &&
-git log --pretty=medium -30 > ./wlc_git_history &&
-docker image build --no-cache --tag $BUILD_TAG . &&
-git checkout working
-echo $BUILD_TAG &&
-docker push $BUILD_TAG
+{
+    git pull &&
+    cd wildlifecompliance/frontend/wildlifecompliance/ &&
+    npm run build &&
+    cd ../../../ &&
+    source venv/bin/activate &&
+    ./manage_wc.py collectstatic --no-input &&
+    git log --pretty=medium -30 > ./wlc_git_history &&
+    docker image build --no-cache --tag $BUILD_TAG . &&
+    git checkout working
+    echo $BUILD_TAG &&
+    docker push $BUILD_TAG
+} || {
+    git checkout working
+    echo 'Build failed'
+}
