3adc50d3832dfdbf49c75814d9dfef79711cd282
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2022-05-18 14:51:39 +0800

    minor

--- make_docker_image_jm.sh
+++ make_docker_image_jm.sh
@@ -22,6 +22,7 @@ npm run build &&
 cd ../../../ &&
 #source venv/bin/activate &&
 #./manage_co.py collectstatic --no-input &&
+git log --pretty=medium -30 > ./git_history_recent &&
 docker image build $NO_CACHE --tag $BUILD_TAG . &&
 echo $BUILD_TAG &&
 docker push $BUILD_TAG
