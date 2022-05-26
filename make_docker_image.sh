759b28ed7ba8a74d9bec936ee227291b3e360503
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-05-26 15:54:23 +0800

    Update build script

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-## first parameter is DBCA branch name, optional second parameter is an integer indicating incremental daily version
+## first parameter is DBCA branch name
+
 set -e
 if [[ $# -lt 1 ]]; then
     echo "ERROR: DBCA branch must be specified"
-    echo "$0 1"
-    exit 1
-fi
-if [[ $# -gt 1 ]] && ! [[ $2 =~ ^[0-9]+$ ]]; then
-    #echo "ERROR: Must specify integer indicating incremental daily version e.g."
-    echo "ERROR: Incremental daily version must be an integer"
     echo "$0 1"
     exit 1
 fi
@@ -17,34 +12,7 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 #REPO=$(basename -s .git `git config --get remote.origin.url` | sed 's/-//g')
 REPO=$(awk '{split($0, arr, "\/"); print arr[2]}' <<< $(git config -l|grep remote|grep url|head -n 1|sed 's/-//g'|sed 's/....$//'))
 DBCA_BRANCH="dbca_"$1
-BUILD_TAG_NO_INCREMENT=dbcawa/$REPO:$1_v$(date +%Y.%m.%d)
-if [[ $# -gt 1 ]]; then
-    INCREMENT=$2
-else
-    INCREMENT=1
-    if [[ $(docker images | awk '{print $1":"$2}' | grep $BUILD_TAG_NO_INCREMENT) ]]; then
-        DAILY_IMAGE_INCREMENTS=$(docker images | awk '{print $1":"$2}' | grep $BUILD_TAG_NO_INCREMENT)
-        declare -i I=0
-        declare -A inc_array
-        for DAILY in $DAILY_IMAGE_INCREMENTS;
-        do
-            #INC=$(echo $DAILY | cut -c $((${#REPO}+21))-)
-            INC=$(echo $DAILY | cut -c $((${#REPO}+${#1}+22))-)
-            inc_array[$I]=$INC
-            I=$(($I+1))
-        done
-        declare -i max_value=0
-        for ii in ${inc_array[@]};
-        do
-            if [ $ii -gt $max_value ]; then
-                max_value=$ii
-            fi;
-        done
-        INCREMENT=$((max_value+1))
-    fi
-fi
-#BUILD_TAG=dbcawa/$REPO:v$(date +%Y.%m.%d).$INCREMENT
-BUILD_TAG=$BUILD_TAG_NO_INCREMENT.$INCREMENT
+BUILD_TAG=dbcawa/$REPO:$1_v$(date +%Y.%m.%d.%H.%M%S)
 {
     git checkout $DBCA_BRANCH
 } ||
@@ -61,8 +29,7 @@ BUILD_TAG=$BUILD_TAG_NO_INCREMENT.$INCREMENT
     npm run build &&
     cd ../../../ &&
     source venv/bin/activate &&
-    #./manage.py collectstatic --no-input &&
-    $(find . -maxdepth 1 -name "manage*.py") collectstatic --no-input &&
+    python manage.py collectstatic --no-input &&
     git log --pretty=medium -30 > ./git_history_recent &&
     docker image build --no-cache --tag $BUILD_TAG . &&
     git checkout $CURRENT_BRANCH
@@ -71,7 +38,6 @@ BUILD_TAG=$BUILD_TAG_NO_INCREMENT.$INCREMENT
 {
     git checkout $CURRENT_BRANCH
     echo "ERROR: Docker build failed"
-    echo "NB: This script assumes that your virtual environment folder is 'venv'"
     echo "$0 1"
     exit 1
 }
