e0305adaf4de0189096c8ff7de8a66e2c7b6caa4
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-06-02 16:31:53 +0800

    Comment out redundant dependency

--- wildlifecompliance/migrations/0601_auto_20220601_1138.py
+++ wildlifecompliance/migrations/0601_auto_20220601_1138.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0034_auto_20220601_1138'),
+        #('accounts', '0034_auto_20220601_1138'),
         ('wildlifecompliance', '0600_remove_region_head_office'),
     ]
 
