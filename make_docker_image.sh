885610a054f36eb890fe8af0ed0fa60d88a09557
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-06-15 11:20:24 +0800

    Add git fetch

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -21,8 +21,9 @@ EXISTING_REMOTES=$(git remote)
         echo "Attempt to create branch"
         echo $REPO
         echo "git remote add $DBCA_ORIGIN_HASH git@github.com:dbca-wa/$REPO.git"
-        git remote add $DBCA_ORIGIN_HASH git@github.com:dbca-wa/$REPO.git
-        git remote set-url --push $DBCA_ORIGIN_HASH no_push
+        git remote add $DBCA_ORIGIN_HASH git@github.com:dbca-wa/$REPO.git &&
+        git fetch $DBCA_ORIGIN_HASH &&
+        git remote set-url --push $DBCA_ORIGIN_HASH no_push &&
         git checkout -b $DBCA_ORIGIN_HASH"_"$1 $DBCA_ORIGIN_HASH"/"$1
     fi
     echo "DBCA branch already exists"
